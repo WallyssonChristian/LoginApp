a54c877a0e940a98d5a45316204df3ff79c46237
Wallysson Christian <Cerberus74@live.com>
2017-10-25 15:59:21 -0200

    adicionado layout e dialog menu

--- app/src/main/java/com/example/administrador/loginapp/ConsultaActivity.java
+++ app/src/main/java/com/example/administrador/loginapp/ConsultaActivity.java
@@ -1,15 +1,20 @@
 package com.example.administrador.loginapp;
 
+import android.app.AlertDialog;
 import android.database.Cursor;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ConsultaActivity extends AppCompatActivity {
     private ListView lista;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,9 +46,42 @@ public class ConsultaActivity extends AppCompatActivity {
 //        lista.setOnLongClickListener(new View.OnLongClickListener() {
 //            @Override
 //            public boolean onLongClick(View v) {
-//                return false;
+//                showOptions();
+//                return true;
 //
 //            }
 //        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                showOptions(position);
+                return true;
+            }
+        });
+
     }
+
+    public void showOptions(int itemPosition){
+        AlertDialog.Builder builder = new AlertDialog.Builder(ConsultaActivity.this);
+        builder.setView(R.layout.layout_dialog);
+
+
+        spinner = (Spinner) findViewById(R.id.spinner_menu);
+
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.spinner_opt, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        } else {
+            Toast.makeText(this, "NoWay", Toast.LENGTH_SHORT).show();
+        }
+
+
+        builder.create();
+        builder.show();
+    }
+
+
 }
